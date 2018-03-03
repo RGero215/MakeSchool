a1c60cf32b5eef1228b72007aff0e6a2fda30035
RGero215 <rgero215@gmail.com>
2018-03-03 08:57:09 -0400

    modified: Grid.swift
    implemented the game logic for Conway's Game of Life.

--- Gameoflife/Gameoflife/Grid.swift
+++ Gameoflife/Gameoflife/Grid.swift
@@ -21,12 +21,24 @@ class Grid: SKSpriteNode {
     var cellWidth = 0
     var cellHeight = 0
     
+    /* Counters */
+    var generation = 0
+    var population = 0
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         /* Called when a touch begins */
         
         /* There will only be one touch as multi touch is not enabled by default */
         let touch = touches.first!
         let location = touch.location(in: self)
+        
+        /* Calculate grid array position */
+        let gridX = Int(location.x) / cellWidth
+        let gridY = Int(location.y) / cellHeight
+        
+        /* Toggle creature visibility */
+        let creature = gridArray[gridX][gridY]
+        creature.isAlive = !creature.isAlive
     }
     
     /* You are required to implement this for your subclass to work */
@@ -55,7 +67,7 @@ class Grid: SKSpriteNode {
         creature.position = gridPosition
         
         /* Set default isAlive */
-        creature.isAlive = true
+        creature.isAlive = false
         
         /* Add creature to grid node */
         addChild(creature)
@@ -78,6 +90,81 @@ class Grid: SKSpriteNode {
                 
                 /* Create a new creature at row / column position */
                 addCreatureAtGrid(x:gridX, y:gridY)
+            }
+        }
+    }
+    
+    func countNeighbors() {
+        /* Process array and update creature neighbor count */
+        
+        /* Loop through columns */
+        for gridX in 0..<columns {
+            
+            /* Loop through rows */
+            for gridY in 0..<rows {
+                
+                /* Grab creature at grid position */
+                let currentCreature = gridArray[gridX][gridY]
+                
+                /* Reset neighbor count */
+                currentCreature.neighborCount = 0
+                
+                /* Loop through all adjacent creatures to current creature */
+                for innerGridX in (gridX - 1)...(gridX + 1) {
+                    
+                    /* Ensure inner grid column is inside array */
+                    if innerGridX<0 || innerGridX >= columns { continue }
+                    
+                    for innerGridY in (gridY - 1)...(gridY + 1) {
+                        
+                        /* Ensure inner grid row is inside array */
+                        if innerGridY<0 || innerGridY >= rows { continue }
+                        
+                        /* Creature can't count itself as a neighbor */
+                        if innerGridX == gridX && innerGridY == gridY { continue }
+                        
+                        /* Grab adjacent creature reference */
+                        let adjacentCreature:Creature = gridArray[innerGridX][innerGridY]
+                        
+                        /* Only interested in living creatures */
+                        if adjacentCreature.isAlive {
+                            currentCreature.neighborCount += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func updateCreatures() {
+        /* Process array and update creature status */
+        
+        /* Reset population counter */
+        population = 0
+        
+        /* Loop through columns */
+        for gridX in 0..<columns {
+            
+            /* Loop through rows */
+            for gridY in 0..<rows {
+                
+                /* Grab creature at grid position */
+                let currentCreature = gridArray[gridX][gridY]
+                
+                /* Check against game of life rules */
+                switch currentCreature.neighborCount {
+                case 3:
+                    currentCreature.isAlive = true
+                    break;
+                case 0...1, 4...8:
+                    currentCreature.isAlive = false
+                    break;
+                default:
+                    break;
+                }
+                
+                /* Refresh population count */
+                if currentCreature.isAlive { population += 1 }
             }
         }
     }
